43279a5252ca26b0d6d96e8b60e0b027bcd04ce7
yoab <cesary.villarrealg@gmail.com>
2020-10-03 12:18:49 -0400

    Folder set up

---
+++ scripts/modeling_script.R
@@ -0,0 +1,15 @@
+##### MODELING SCRIPT
+# File contains code used to generate data used 
+# for the report "report.pdf", before running this
+# code make sure you have installed all the libraries used
+# TO use this file properly follow the instructions
+# in the README.md file located inside the 'scripts' folder.
+
+### LIBS ####
+
+
+#### GENERATING DATA ####
+
+
+#### CLEAN UP ####
+rm(list = ls())
