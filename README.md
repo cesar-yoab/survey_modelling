085246ca183c8cfc98b6201d925dc99c22515e4f
yoab <cesary.villarrealg@gmail.com>
2020-10-04 13:19:34 -0400

    Update to python script and README

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Survey can be found [here](https://cesarvillarrealg.typeform.com/to/XX9vwSu5)
 # Sources
 1. [Population](https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=1710000901)
 2. [List of users](https://randomuser.me/api/?results=5000)
+3. [YouTube API](https://developers.google.com/youtube/v3/docs/videos/list)
+4. [YouTube API keys](https://developers.google.com/youtube/v3/docs/?apix=true)
--- scripts/data_gen.py
+++ scripts/data_gen.py
@@ -1,9 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 import os
-import json
 import click
-import time
+import json
+
+
+# List of videos to get information
+vids = ['SIPCAhS0xjw', 'rQgQJ2kYnqo',
+        'FSOZrt3tTho', 'x0baobXKkIM', 'UVuk-qGQb2A']
 
 
 def parse_with_bs4(page):
@@ -50,7 +54,25 @@ def write_data(data, filename, path):
             f.write(k + ':' + v + '\n')
 
 
-def write_profiles(data, filename, path):
+def call_api(key):
+    """Generates query for the API and calls the API"""
+    url = 'https://www.googleapis.com/youtube/v3/videos?part=statistics&id='
+
+    for v in vids:
+        url += v + ','
+
+    url = url.rstrip() + "&key=" + key
+
+    # Call the API
+    resp = requests.get(url)
+
+    if resp.status_code == 200:
+        return resp.json()
+
+    return {}
+
+
+def write_response(data, filename, path):
     """ Write json data to json file."""
 
     # Full path for new json file
@@ -60,37 +82,18 @@ def write_profiles(data, filename, path):
         json.dump(data, f, ensure_ascii=False, indent=2)
 
 
-def get_profiles(api_url, params):
-    """Call profiles API 2 times to create 10,000 fake
-    samples."""
-    api_call = api_url + '?'
-
-    for k, v in params.items():
-        api_call += k + '=' + str(v) + '&'
-
-    resp = requests.get(api_call.rstrip())
-
-    if resp.status_code == 200:
-        return resp.json()['results']
-
-    print("Call to API returned status code: ", resp.status_code)
-    return []
-
-
 @click.command()
 @click.option('--dir', default=os.getcwd(),
               type=click.Path(exists=True),
               help='Full path of directory to store data.')
-@click.option('--no-profiles', is_flag=True, help="Not include profiles file.")
+@click.option('--api-key', help="Your unique YouTube API key.", default=False)
 @click.option('--no-provinces', is_flag=True, help="Not include provinces file.")
-def main(dir, no_profiles, no_provinces):
-    """Script to generate data for modelling survey responses.
-        Script generates a json file with 10,000 randomly generated fake profiles,
-        and a txt file with Canadas provinces and corresponding populations.\n
-        Note: You cannot use both the --no-profiles and --no-provinces flags simultaneously."""
-    if no_profiles and no_provinces:
+def main(dir, api_key, no_provinces):
+    """Script to crawl current province census and gather data for modelling from the YouTube API.
+    A key is required."""
+    if not api_key:
         ctx = click.get_current_context()
-        ctx.fail("You cannot select both flags.")
+        ctx.fail("You need to include your API key to call the YouTube API")
         return
 
     # Code to generate provinces file
@@ -103,22 +106,8 @@ def main(dir, no_profiles, no_provinces):
 
             write_data(data, 'provinces.txt', os.getcwd())
 
-    # Code to generate fake profile list
-    if not no_profiles:
-        api_url = 'https://randomuser.me/api/'
-        # Ask for 5000 results each with Canadian nationality
-        params = {'results': 5000, 'nat': 'ca',
-                  'exc': 'login,registered,picture'}
-
-        # The API only allows 5000 samples per call, so we make two
-        # and merge them into a single dictionary
-        p1 = get_profiles(api_url, params)
-        time.sleep(2)
-        p2 = get_profiles(api_url, params)
-
-        profiles = {'results': p1 + p2}
-
-        write_profiles(profiles, 'profiles.json', os.getcwd())
+    data = call_api(api_key)
+    write_response(data, "yt-response.json", dir)
 
 
 if __name__ == "__main__":
